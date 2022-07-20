8d7cc208b556218d1598588035515b56557e3f9f
Annalartey <74235952+Annalartey@users.noreply.github.com>
2022-07-20 22:37:44 +0000

    updated dark mode

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+let body = document.getElementById("body");
+let container = document.getElementById("container");
 let input = document.getElementById("input");
 let operation = document.querySelectorAll(".operation div");
 let number = document.querySelectorAll(".numbers div");
 let deleteButton = document.getElementById("delete");
 let clear = document.getElementById("clear");
 let result = document.getElementById("result");
+let themeDark = document.getElementById("theme_dark")
+let themeLight = document.getElementById("theme_light")
+let dot = document.getElementById("dot")
 resultDisplayed = false;
 
 
@@ -55,3 +60,39 @@ clear.onclick = () => {
 result.onclick = () => {
     input.innerHTML = eval(input.innerHTML); 
 } 
+
+themeDark.onclick = () => {
+    body.style.backgroundColor = "black"
+    body.style.color = "gray"
+    container.style.backgroundColor = "black"
+ 
+    for (i = 0; i < number.length; i++){
+        number[i].style.backgroundColor = "#1E5631"
+    }
+
+    for (i = 0; i <operation.length; i++){
+        operation[i].style.backgroundColor = "#210D26"
+    }
+
+    deleteButton.style.backgroundColor="#210D26"
+    result.style.backgroundColor = "#400740"
+    clear.style.backgroundColor = "#210D26"
+    dot.style.backgroundColor = "#210D26"
+
+}
+themeLight.onclick = () =>{
+    body.style.backgroundColor = null;
+    body.style.color = null;
+    container.style.backgroundColor = null;
+ 
+    for (i = 0; i < number.length; i++){
+        number[i].style.backgroundColor = null;
+    }
+
+    for (i = 0; i <operation.length; i++){
+        operation[i].style.backgroundColor = null;
+    }
+
+    deleteButton.style.backgroundColor=null;
+    result.style.backgroundColor = null;
+}
